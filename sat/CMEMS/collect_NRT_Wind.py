b4e29c06440942ce174e4efbd6cdb1cb3f209440
norlandrhagen <norlandrhagen@gmail.com>
2021-06-29 10:16:35 -0700

    added argo collect, wind and cmore_bula

--- sat/CMEMS/collect_NRT_Wind.py
+++ sat/CMEMS/collect_NRT_Wind.py
@@ -3,20 +3,21 @@ import os
 
 """
 
-FINISHED --2021-06-15 16:53:37--
-Total wall clock time: 5h 45m 21s
-Downloaded: 5085 files, 51G in 5h 1m 57s (2.86 MB/s)
-
+FINISHED --2021-06-16 16:45:22--
+Total wall clock time: 5h 33m 30s
+Downloaded: 5089 files, 51G in 4h 47m 28s (3.01 MB/s)
 """
-output_dir = vs.collected_data + "sat/CMEMS_NRT_Wind/"
-output_dir = output_dir.replace(" ", "\\ ")
+
+
+output_dir = (vs.collected_data + "sat/CMEMS_NRT_Wind/").replace(" ", "\\ ")
+
 ftp_link = "ftp://nrt.cmems-du.eu/Core/WIND_GLO_WIND_L4_NRT_OBSERVATIONS_012_004/CERSAT-GLO-BLENDED_WIND_L4-V6-OBS_FULL_TIME_SERIE/*"
 
 
 def wget_file(fpath, output_dir, usr, psw):
     os.system(
-        f"""wget -nd -r -m --ftp-user={usr} --ftp-password={psw} {fpath}  -P  {output_dir}"""
+        f"""wget --no-parent -nd -r -m --ftp-user={usr} --ftp-password={psw} {fpath}  -P  {output_dir}"""
     )
 
 
-wget_file(ftp_link, output_dir, "{insert_usr", "insert_psw")
+wget_file(ftp_link, output_dir, {usr}, {psw})
